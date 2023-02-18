ee8f9d09550b64bcea3b20eaea5d5c9c2cb2895b
Tushar	 Gupta <tushar.gupta@Tushar-Gupta.local>
2023-02-18 12:21:13 +0530

    added URL shortner app

---
+++ Week-10/shortly/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Col, Row } from "react-bootstrap";
+function Header() {
+  return (
+    <Row className="header-row">
+      <Col>
+        <h1 className="header-text">Shortly</h1>
+      </Col>
+    </Row>
+  );
+}
+export default Header;
